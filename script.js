6ca1b2db97c5a9aaf827997d2ad06ceca02afd8a
Damian Ramos <damianramos225@gmail.com>
2020-02-18 18:16:29 -0500

    updated

---
+++ script.js
@@ -0,0 +1,50 @@
+$(document).on("click", "#searchCity", function () {
+
+    var city = $(this).attr("id", "searchCity");
+    console.log(city);
+
+    // retreive data from local storage
+
+
+    // convert to an array
+
+
+    // push new city into that array
+
+
+    // save to local storage
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=London,us&appid=ea1ee909c94c46ccd1e4885c0e999ea2";
+
+
+
+
+
+
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+        $("#weatherfore").text(JSON.stringify(response.list[0].weather));
+    });
+
+
+
+
+
+
+})
+
+// retreive infor from local storage
+var storedHistory = localStorage.getItem('weather')
+// convert data to a obj or array JSON.parse()
+// put info from localstorage onto the page
+
+console.log("hello world")
+
+
+
+
