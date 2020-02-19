6c5413545ac8592bbe7713d902f208cac5f09e41
Damian Ramos <damianramos225@gmail.com>
2020-02-18 20:53:25 -0500

    made-changes

--- script.js
+++ script.js
@@ -3,24 +3,13 @@ $(document).on("click", "#searchCity", function () {
     var city = $(this).attr("id", "searchCity");
     console.log(city);
 
-    // retreive data from local storage
-
 
     // convert to an array
 
 
     // push new city into that array
 
-
-    // save to local storage
-
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=London,us&appid=ea1ee909c94c46ccd1e4885c0e999ea2";
-
-
-
-
-
-
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=Houston,us&appid=ea1ee909c94c46ccd1e4885c0e999ea2";
 
 
     $.ajax({
@@ -34,13 +23,24 @@ $(document).on("click", "#searchCity", function () {
 
 
 
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=Newyork,us&appid=ea1ee909c94c46ccd1e4885c0e999ea2";
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+        $("#weatherfore").text(JSON.stringify(response.list[0].weather));
+    });
 
 
 })
 
 // retreive infor from local storage
-var storedHistory = localStorage.getItem('weather')
+var storedHistory = localStorage.getItem('weatherfore')
 // convert data to a obj or array JSON.parse()
+
 // put info from localstorage onto the page
 
 console.log("hello world")
